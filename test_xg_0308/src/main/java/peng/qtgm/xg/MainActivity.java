e4c0d6125a1b8093d6f9d56dfc4173584c60d9c4
HanJingZhe <386800265@qq.com>
2019-03-08 19:20:45 +0800

    添加本地推送

--- test_xg_0308/src/main/java/peng/qtgm/xg/MainActivity.java
+++ test_xg_0308/src/main/java/peng/qtgm/xg/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.tencent.android.tpush.XGLocalMessage;
+import com.tencent.android.tpush.XGPushConfig;
 import com.tencent.android.tpush.XGPushManager;
 import com.tencent.android.tpush.XGPushRegisterResult;
 
@@ -24,6 +26,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void push() {
+
+        /**
+         *  XGPushManager         Push服务推送
+         *  XGPushConfig          Push服务配置项接口
+         *  XGPushBaseReceiver    接收消息和结果反馈的receiver，需要开发者自己在AndroidManifest.xml静态注册
+         */
+        //新建本地通知
+        XGLocalMessage localMsg = new XGLocalMessage();
+        localMsg.setType(1);
+        localMsg.setTitle("HELLO WORLD");
+        localMsg.setContent("本地推送的内容");
+        localMsg.setDate("20140930");//设置日期
+        //设置触发的时间
+        localMsg.setHour("19");
+        localMsg.setMin("31");
+
+        //设置动作类型：1打开activity或app本身，2打开浏览器，3打开Intent ，4通过包名打开应用
+        localMsg.setAction_type(1);
+        localMsg.setActivity("peng.qtgm.xg.AssignActivity");
+        localMsg.setUrl("http://www.baidu.com");
+
+        // 设置下载应用URL
+        //localMsg.setPackageDownloadUrl("http://softfile.3g.qq.com:8080/msoft/179/1105/10753/MobileQQ1.0(Android)_Build0198.apk");
+
+        XGPushManager.addLocalNotification(this,localMsg);
 
     }
 
