5cc6903ce39a92b50e1c611d55312b3cd07cd3ad
HanJingZhe <386800265@qq.com>
2019-04-26 18:11:20 +0800

    new agora module

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ t0309_video_compress : 视频压缩
 t0330_recycler : explore RecyclerView的分组用法 、 右滑删除 拖拽功能 、流布局recycler
 t0424_lambda: 练习lambda表达式
 t0426_preview: 类似iOS页面侧滑返回
+t0426_agora: 练习声网视频SDK
--- t0426_preview/src/main/java/peng/qtgm/t0426_preview/MainActivity.java
+++ t0426_preview/src/main/java/peng/qtgm/t0426_preview/MainActivity.java
@@ -2,12 +2,28 @@ package peng.qtgm.t0426_preview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.blankj.swipepanel.SwipePanel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipePanel swipePanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipePanel = findViewById(R.id.swipePanel);
+        swipePanel.setOnFullSwipeListener(new SwipePanel.OnFullSwipeListener() {// 设置完全划开松手后的监听
+            @Override
+            public void onFullSwipe(int direction) {
+                finish();
+                swipePanel.close(direction);// 关闭
+            }
+        });
+
+
     }
 }
