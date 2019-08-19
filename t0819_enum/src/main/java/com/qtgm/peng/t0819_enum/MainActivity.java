d6184b4ad99c3ac8ec41052fbf8ac54325b76780
peng_wang <qtgm386800265@163.com>
2019-08-19 11:46:04 +0800

    add enum module

---
+++ t0819_enum/src/main/java/com/qtgm/peng/t0819_enum/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qtgm.peng.t0819_enum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ t0819_enum/src/main/java/com/qtgm/peng/t0819_enum/constant/GoclassStateEnum.java
@@ -0,0 +1,9 @@
+package com.qtgm.peng.t0819_enum.constant;
+
+/**
+ * @author peng_wang
+ * @date 2019/8/19
+ */
+public enum GoclassStateEnum {
+    HOLD,JUST,FINISH
+}
